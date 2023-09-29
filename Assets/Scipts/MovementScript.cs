5300bf49bf680bccf951c970f3082a44e699b985
JDBryan <47214792+JDBryan@users.noreply.github.com>
2023-09-29 04:27:47 -0700

    feat: Add basic player movement

---
+++ Assets/Scipts/MovementScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        transform.Translate(movement * Time.deltaTime);
+    }
+}
+
