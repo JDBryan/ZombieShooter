c70c09378af4af30ad188f791e76f681c0b383da
JDBryan <47214792+JDBryan@users.noreply.github.com>
2023-09-29 05:07:27 -0700

    Add player mouse movement

--- Assets/Scipts/MovementScript.cs
+++ Assets/Scipts/MovementScript.cs
@@ -7,7 +7,7 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Rotate(0,0,270);
     }
 
     // Update is called once per frame
@@ -15,10 +15,22 @@ public class Movement : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+		
+		//Get the Screen position of the mouse and player
+        Vector2 playerPosition = Camera.main.WorldToViewportPoint(transform.position);
+		Vector2 mousePosition = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		
+		//Get the angle between the player and the camera
+		float angle = AngleBetweenTwoPoints(playerPosition, mousePosition);
 
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-
-        transform.Translate(movement * Time.deltaTime);
+		//Perform translation and rotation
+		transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle+90));
+        transform.position += movement * Time.deltaTime;
     }
+
+    float AngleBetweenTwoPoints(Vector2 a, Vector2 b) {
+		return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+	}
 }
 
