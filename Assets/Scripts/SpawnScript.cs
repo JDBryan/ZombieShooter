e1a7e343f56de04de24a7dc70df9b2693026bb10
joachim boyden <joachimboyden@gmail.com>
2023-10-12 20:29:02 +0100

    finished spawner

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class SpawnScript : MonoBehaviour
 {
-    public float timeBetweenWave = 5f;
-    private int enemyNumber = 1;
+    private float timeBetweenSpawn = 2f;
+    private int enemyNumber = 5;
     private float countdown = 5f;
     private Vector3 spawnerSize;
     private MeshRenderer myRenderer;
     public GameObject newEnemy;
+    public GameObject controller;
 
 
     // Start is called before the first frame update
@@ -17,18 +18,38 @@ public class SpawnScript : MonoBehaviour
     {
         myRenderer = GetComponent<MeshRenderer>();
         spawnerSize = GetComponent<Renderer>().bounds.extents;
-
+        countdown = timeBetweenSpawn;
+        enemyNumber = controller.GetComponentInParent<WaveControllerScript>().waveAmount;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (controller.GetComponentInParent<WaveControllerScript>().waveOn != true){
+            enemyNumber = controller.GetComponentInParent<WaveControllerScript>().waveAmount;
+        }
+        else{
+            Wave();
+        }   
+    }
+
+    void Spawn()
+    {
         if (countdown <= 0){
             GameObject enemy = Instantiate(newEnemy, FindSpawnPoint(), this.transform.rotation);
-            countdown = timeBetweenWave;
+            enemyNumber -= 1;
+            countdown = timeBetweenSpawn;
         }
         else{
             countdown -= Time.deltaTime;
+        }
+    }
+
+    void Wave()
+    {
+        if (enemyNumber > 0){
+            
+            Spawn();
         }
     }
 
@@ -37,7 +58,7 @@ public class SpawnScript : MonoBehaviour
         float minX = this.transform.position.x-spawnerSize.x;
         float maxX = this.transform.position.x+spawnerSize.x;
         float minY = this.transform.position.y-spawnerSize.y;
-        float maxY = this.transform.position.y-spawnerSize.y;
+        float maxY = this.transform.position.y+spawnerSize.y;
         Vector3 spawnPoint = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
 
         return spawnPoint;
