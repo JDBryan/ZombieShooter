d82b6be0dcd635753c8eda05fb4126d589eafe8b
joachim boyden <joachimboyden@gmail.com>
2023-11-01 21:59:57 +0000

    New health bar

--- Assets/Scripts/UserInterfaceScript.cs
+++ Assets/Scripts/UserInterfaceScript.cs
@@ -10,13 +10,22 @@ public class UserInterface : MonoBehaviour
     [SerializeField] private TMP_Text waveNumberText;
     [SerializeField] private TMP_Text healthBar;
     [SerializeField] private GameObject deathScreen;
+    [SerializeField] private GameObject ActiveWeapon;
+    [SerializeField] private GameObject SecondaryWeapon;
+    [SerializeField] private Sprite PistolSprite;
+    [SerializeField] private Sprite ShotgunSprite;
+    [SerializeField] private GameObject HealthBarMask;
 
     public void SetWaveNumber(int waveNumber) {
         waveNumberText.SetText(waveNumber.ToString());
     }
 
     public void SetHealthBar(int health) {
-        healthBar.SetText(health.ToString());
+        if (health >= 0){
+            float healthPercent = (float)health / (float)100;
+            float newHealthPosition = healthPercent * 7.76f;
+            HealthBarMask.transform.SetLocalPositionAndRotation(new Vector3(newHealthPosition, 0f, 0f), HealthBarMask.transform.rotation);
+        }
     }
 
     public void SetWeaponInfoText(Weapon activeWeapon) {
@@ -24,7 +33,7 @@ public class UserInterface : MonoBehaviour
         weaponInfoText.SetText(activeWeapon.name + ": " + ammoText);
     }
 
-    public void EnableGameOverScreen() {
+public void EnableGameOverScreen() {
         Debug.Log("ENDING GAME");
         this.deathScreen.SetActive(true);
     }
