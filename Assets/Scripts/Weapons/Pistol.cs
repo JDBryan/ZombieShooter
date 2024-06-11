9e9a5c2ff4f5c6aae354d73d59a4c337de19a9d4
joachim boyden <joachimboyden@gmail.com>
2024-06-11 19:01:05 +0100

    feat: Added reload bar and weapon properties

--- Assets/Scripts/Weapons/Pistol.cs
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -1,86 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Pistol : Weapon
 {
-
-    void Start() {
-        // Weapon parameters
-        this.isAutomatic = false;
-        this.fireRate = 0.5f;
-        this.clipSize = 12;
-        this.hasInfiniteAmmo = true;
-        this.initialAmmoCount = 0;
-        
-        // Weapon tracking
-        this.currentAmmoCount = this.initialAmmoCount;
-        this.roundsLeftInClip = this.clipSize;
-        this.lastRoundFiredTime = Time.time;
-        this.triggerHeld = false;
-        this.roundsFiredWhileTriggerHeld = 0;
-
-        // Sprite rendering
-        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
-        this.userInterface = FindObjectOfType<UserInterface>();
-    }
-
-    void LateUpdate() {
-        float currentTime = Time.time;
-        if ((this.triggerHeld && currentTime - this.lastRoundFiredTime >= this.fireRate)
-            && (this.isAutomatic || this.roundsFiredWhileTriggerHeld == 0)) 
-        {
-            this.Fire();
-            this.roundsFiredWhileTriggerHeld += 1;
-        }
-    }
-
-    public override void PullTrigger()
-    {
-        this.triggerHeld = true;
-    }
-
-    public override void ReleaseTrigger()
-    {
-        this.triggerHeld = false;
-        this.roundsFiredWhileTriggerHeld = 0;
-    }
-
     public override void Fire()
     {
+        // Fire round
         Transform player = this.transform.parent;
-        float currentTime = Time.time;
-        if (currentTime - this.lastRoundFiredTime >= this.fireRate)
-        {
-            this.lastRoundFiredTime = currentTime;
-            if (this.roundsLeftInClip > 0) {
-                this.ChangeSpriteToFire();
-                GetComponent<AudioSource>().PlayOneShot(fireSound);
-                GameObject bullet = Instantiate(bulletPrefab, player.position, player.rotation);
-                Physics2D.IgnoreCollision(bullet.GetComponent<BoxCollider2D>(), player.GetComponent<CircleCollider2D>());
-                Rigidbody2D bulletBody = bullet.GetComponent<Rigidbody2D>();
-                bulletBody.velocity = bulletBody.transform.up * 40;
+        this.ChangeSpriteToFire();
+        GetComponent<AudioSource>().PlayOneShot(fireSound);
+        GameObject bullet = Instantiate(bulletPrefab, player.position, player.rotation);
+        bullet.transform.eulerAngles = new Vector3(
+                bullet.transform.eulerAngles.x,
+                bullet.transform.eulerAngles.y,
+                bullet.transform.eulerAngles.z + Random.Range(-this.bulletSpread, this.bulletSpread)
+            );
+        Physics2D.IgnoreCollision(bullet.GetComponent<BoxCollider2D>(), player.GetComponent<CircleCollider2D>());
+        Rigidbody2D bulletBody = bullet.GetComponent<Rigidbody2D>();
+        bulletBody.velocity = bulletBody.transform.up * this.bulletSpeed;
+        
+        // Update weapon state
+        this.lastRoundFiredTime = Time.time;
+        this.roundsFiredWhileTriggerHeld += 1;
+        this.roundsLeftInClip -= 1;
 
-            if (this.hasInfiniteAmmo){
-                this.roundsLeftInClip -= 1;
-                userInterface.UpdateWeaponInfo();
-            }
-
-            }
-            if (!this.hasInfiniteAmmo && this.currentAmmoCount > 0 && this.roundsLeftInClip > 0) {
-                this.currentAmmoCount -= 1;
-                this.roundsLeftInClip -= 1;
-                userInterface.UpdateWeaponInfo();
-            }
-        }
-    }
-
-    public override void ChangeSpriteToFire(){
-        gameObject.GetComponent<SpriteRenderer>().sprite = this.fireSprite;
-    }
-
-    public override void ChangeSpriteToIdle(){
-        gameObject.GetComponent<SpriteRenderer>().sprite = this.idleSprite;
+        // Update user interface
+        userInterface.UpdateWeaponInfo();
     }
 }
 
